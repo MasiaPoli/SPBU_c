7b5fccf78a13d47c678225bb6072096699d5408e
MasiaPoli <126983383+MasiaPoli@users.noreply.github.com>
2023-10-29 18:34:56 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -4,13 +4,17 @@
 int main()
 {
    int x, y;
+   printf("Write a number, we will write sum of its digits\n");
    scanf("%d", &x);
    printf("%d\n", st_sumn(x));
+   printf("Write a number, we will write its digital root\n");
    scanf("%d", &x);
    printf("%d\n", st_digroot(x));
+   printf("Write two numbers, we will write their greatest common divider two times\n");
    scanf("%d%d", &x, &y);
    printf("%d\n", st_gcd_i(x, y));
    printf("%d\n", st_gcd_r(x, y));
+   printf("Write two numbers, we will write their  least common multiple\n");
    scanf("%d%d", &x, &y);
    printf("%d\n", st_lcm(x, y));
     return 0;
